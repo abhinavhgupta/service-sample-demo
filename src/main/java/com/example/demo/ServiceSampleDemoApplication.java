2f32dc1d77f6665d2f1149987e442743def98c89
Abhinav Gupta <Abhinav.Gupta>
2020-04-28 09:03:39 +0530

    initial commit

---
+++ src/main/java/com/example/demo/ServiceSampleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSampleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSampleDemoApplication.class, args);
+	}
+
+}
